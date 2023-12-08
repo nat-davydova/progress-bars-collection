c40865fe5972af487adf8d7414c5b0c504e89d3f
nat-davydova <natti.davydova@gmail.com>
2023-12-08 20:52:41 +0300

    example 1: outsource common styles from example #1

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,8 @@ setProgressBars({
   parentSelector: ".example-1",
   className: "example-1__progress",
 });
+
+setProgressBars({
+  parentSelector: ".example-2",
+  className: "example-2__progress",
+});
